96dbb2523c1cb7b9cd31ca8e6261a88a4bf7a5cc
Michal Salášek <485342@muni.cz>
2020-07-05 18:44:47 +0200

    Setup database and MVVP
    Setup local database and MVVP environment
    using caliburn.micro.

---
+++ TMDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMDesktopUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
